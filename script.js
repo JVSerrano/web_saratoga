3a7c0fff9478ce8eb6345538fbb3a766425ae804
Javier Valenzuela <jabato1991@gmail.com>
2024-12-22 20:34:17 +0100

    contratacion acabada a falta de tipografia

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ document.addEventListener('DOMContentLoaded', function() {
         navbarMenu.classList.toggle('active');
     });
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const hiringToggle = document.querySelector('.hiring-toggle');
+    const hiringMenu = document.querySelector('.hiring-menu');
+
+    hiringToggle.addEventListener('click', function() {
+        hiringMenu.classList.toggle('active');
+    });
+});
