bcb47a6405863c0c5193c78469aa9262aadb5f4f
Javier Valenzuela <jabato1991@gmail.com>
2024-12-25 12:13:40 +0100

    implantacion de botones en bio, por acabar formaciones

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const hamburger = document.querySelector('.hamburger-menu');
     const navbarMenu = document.querySelector('.navbar_menu');
 
-    hamburger.addEventListener('click', function() {
+    hamburger.addEventListener('click', function () {
         navbarMenu.classList.toggle('active');
     });
 });
 
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const hiringToggle = document.querySelector('.hiring-toggle');
     const hiringMenu = document.querySelector('.hiring-menu');
 
-    hiringToggle.addEventListener('click', function() {
+    hiringToggle.addEventListener('click', function () {
         hiringMenu.classList.toggle('active');
     });
 });
+
+
+let btnBio = document.querySelector('.btn-bio')
+let containerBio = document.querySelector('.div-bio')
+containerBio.style.display = 'block'
+
+let btnLineup = document.querySelector('.btn-lineup')
+let containerLineup = document.querySelector('.div-lineup')
+containerLineup.style.display = 'none'
+
+
+
+function changeBio() {
+    containerBio.style.display = 'block'
+    containerLineup.style.display = 'none'
+}
+
+function changeLineup() {
+    containerBio.style.display = 'none'
+    containerLineup.style.display = 'block'
+}
